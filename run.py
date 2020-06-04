8e45e56cb53c1c5e20297a206da6c290ee19ed65
an-slua-sidhe <polocoinn@yahoo.ie>
2020-06-04 15:43:20 +0100

    Add a multitude of files
    Updated .gitignore, installed 'clean theme' for Booststrap 4, created several html files as well as base.html for Flask.

--- run.py
+++ run.py
@@ -1,15 +1,27 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello, World"
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=os.environ.get("PORT"),
-            # Do not use 'debug=True' in actually applications, only when testing, like here! :)
+            # Do not use 'debug=True' in actual applications, only when testing, like here! :)
             debug=True)
